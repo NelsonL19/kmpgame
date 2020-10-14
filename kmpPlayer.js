af61f6f036f4299da898a23822d5de537283ebd4
Sam <samuelmi@live.unc.edu>
2020-10-13 21:36:31 -0400

    Tomfoolery

---
+++ kmpPlayer.js
@@ -0,0 +1,11 @@
+function kmpPlayer(node) {
+    this.node = node; 
+
+    let move = function(direction) {
+        switch (direction) {
+            case direction.UP:
+                
+        }
+    }
+
+}
