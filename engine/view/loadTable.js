a459c5b5d7f981980ddeed81fc21e0663dcce796
amjpro <46569453+amjpro@users.noreply.github.com>
2020-11-06 20:48:58 -0500

    Adding Folders to Github

---
+++ engine/view/loadTable.js
@@ -0,0 +1,63 @@
+let randomtable = Math.floor(Math.random()*4);
+let tablenum = randomtable
+
+export function tableGen () {
+    switch(tablenum){
+        case 0:
+            
+        break;
+        case 1:
+        break;
+        case 2:
+        break;
+        case 3:
+        break;
+    }
+    for (let i = 1; i < 226; i++) {
+
+    }
+}
+
+export function loadTableDOM() {
+    //loads walls of table
+    for (let i = 1; i < 16; i++) {
+        $(`.c${i}`).addClass('wall');
+    }
+    for (let i = 1; i < 212; i+=15) {
+        $(`.c${i}`).addClass('wall');
+    }
+    for (let i = 15; i < 226; i+=15) {
+        $(`.c${i}`).addClass('wall');
+    }
+    for (let i = 211; i < 226; i++) {
+        $(`.c${i}`).addClass('wall');
+    }
+    tableGen();
+}
+
+export function loadSpecificTableDOM(board) {
+    for (i = 0; i < 225; i++) {
+        if (board[i] == "w") {
+
+        }
+    }
+}
+
+let board0 = 
+[
+"w","w","w","w","w","w","w","w","w","w","w","w","w","w","w",
+"w","e","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","a","a","a","a","a","a","a","a","a","a","a","a","a","w",
+"w","w","w","w","w","w","w","w","w","w","w","w","w","w","w"
+];
