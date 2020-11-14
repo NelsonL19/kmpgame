e7aefd0282d1d9f8085dbab34359f96f0d819d8b
Sam <samuelmi@live.unc.edu>
2020-11-14 10:57:30 -0500

    YEEE DAWGY GOT 'ER TO WORK (mostly)

--- public/javascripts/loginScript.js
+++ public/javascripts/loginScript.js
@@ -1,11 +1,5 @@
 $(function () {
-    const socket = io('kmpunc.com');
-    
     let $username = $('#username');
     let $submit = $('#submit');
 
-    $submit.on("click", () => {
-        alert($username.val());
-        socket.emit('username set', $username.val()); // Tells server that a user submitted their screen name
-    });
 });
