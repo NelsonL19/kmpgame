3a635aa6a9ae66ad4c2a351758bd2332d52e71fc
Nelson Lopez <43620036+NelsonL19@users.noreply.github.com>
2020-11-17 19:58:10 -0500

    setting port to automatically

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ io.on('connection', async (socket) => { // Listens for a new user (represented b
     })
 });
  
-http.listen(5000, () => {
+http.listen(process.env.PORT, () => {
     console.log("Listening");
 });
 
